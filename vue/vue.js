248b4872fa2132e92700fb08acfe95c6feb9b454
Maxim <maxidromlordus@gmail.com>
2021-03-07 20:09:16 +0300

    Vue8 lessons

--- vue/vue.js
+++ vue/vue.js
@@ -1,20 +1,70 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    message: "VUE FIRST EXEMPLE",
-    description: "Learning is fun",
-    image: "./assets/green.png",
-    link: "vue.css",
-    grass: false,
-    sizes: ["big", "medium", "small"],
-    colors: [
-      { id: 1, color: "green", image: "./assets/green.png" },
-      { id: 2, color: "blue", image: "./assets/blue.png" }
-    ],
-    times: 0,
-    disabled: false
+Vue.component("list", {
+  props: {
+    sizes: {
+      type: Array,
+      required: true
+    }
   },
+  template: `
+  <ul>
+    <li v-for="size in sizes">{{size}}</li>
+  </ul>`
+});
 
+Vue.component("sky", {
+  template: `
+  <div class="body">
+    <div class="image"><img v-bind:src="image" /></div>
+    <div class="information">
+      <a v-bind:href="link">OPEN CSS FILE</a>
+      <p v-if="grass">GRASS</p>
+      <p v-else>{{title}}</p>
+
+    <div
+      v-for="(color, index) in colors"
+      :key="colors.id"
+      :style="{backgroundColor: color.color}"
+      class="types-of-images"
+      @mouseover="changeColor(index)"
+    ></div>
+    <button
+      class="button"
+      @click="incrementCoutner"
+      :disabled="times > 9"
+      :class="{'disabled-button':disabled}"
+    >
+      Increment Button
+    </button>
+    <p>counter: {{times}}</p>
+  </div>
+</div>`,
+  data() {
+    return {
+      message: "VUE FIRST EXEMPLE",
+      lookAt: "SKY",
+      looksLike: "BLUE",
+      selectedColorIndex: 0,
+      description: "Learning is fun",
+      link: "vue.css",
+      grass: false,
+      colors: [
+        {
+          id: 1,
+          color: "green",
+          image: "./assets/green.png",
+          disabledButton: true
+        },
+        {
+          id: 2,
+          color: "blue",
+          image: "./assets/blue.png",
+          disabledButton: false
+        }
+      ],
+      times: 0,
+      disabled: false
+    };
+  },
   methods: {
     incrementCoutner: function() {
       this.times += 1;
@@ -22,8 +72,27 @@ var app = new Vue({
         this.disabled = true;
       }
     },
-    changeColor: function(colorImage) {
-      this.image = colorImage;
+    changeColor: function(index) {
+      this.selectedColorIndex = index;
     }
+  },
+
+  computed: {
+    title() {
+      return `${this.lookAt} is ${this.looksLike}`;
+    },
+    image() {
+      return this.colors[this.selectedColorIndex].image;
+    },
+    disabledButton() {
+      return this.colors[this.selectedColorIndex].disabledButton;
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    sizes: ["small", "medium", "big"]
   }
 });
