22c7c6bb22e897449a961415d1884993ec38ca68
Maxim <maxidromlordus@gmail.com>
2021-03-21 09:34:07 +0300

    first atemt

--- Project-3/project.js
+++ Project-3/project.js
@@ -1,26 +1,68 @@
 // Discribe class Elevator
 
+class MyErrors {
+  constructor(type) {
+    this.type = type;
+  }
+
+  toString() {
+    console.log(`${this.type} ERROR`);
+  }
+}
+
+let wrongTypeError = new MyErrors("type");
+
 class Elevator {
-  constructor(position = 0, basement = true, penthouse = true) {
+  constructor(
+    state = staing,
+    position = 0,
+    basement = true,
+    penthouse = true,
+    floorsToMove = 0
+  ) {
+    this.state = state;
+    this.position = position;
     this.basement = basement;
     this.penthouse = penthouse;
-    this.position = position;
+    this.floorsToMove = floorsToMove;
   }
 
-  audioMessage() {
-    this.position = this.destination;
-    console.log(this.position);
-  }
-  goToFloor(destination) {
-    let seconds =
-      this.position > this.destination
-        ? this.position - this.destination
-        : this.destination - this.position;
-    setTimeout(audioMessage, seconds);
+  myCurrentPossition(floorsToMove, position) {
+    this.florsToMove = floorsToMove -= 1;
+    this.position = position += 1;
+    console.log(floorsToMove, position);
   }
 
-  currentposition() {
-    return this.position;
+  addDestination(destination) {
+    if (!this.destinations) {
+      this.destinations = new Array();
+    }
+    console.log(typeof this.destinations);
+    this.destinations.push(destination);
+    this.destinations.sort();
+    console.log(this.destinations);
+    this.moveUp(this.destinations[0]);
+    this.state = "moving";
+  }
+
+  removeDestination(destination) {
+    this.destinations.pop(destination);
+    this.destinations.sort();
+    this.position = destination;
+    if (destinations.length > 0) {
+      moveUp(this.destinations[0]);
+      this.state = "moving";
+    } else {
+      this.state = "staing";
+    }
+  }
+
+  moveUp(destination) {
+    do {
+      this.position += 1;
+    } while (this.position !== destination);
+    console.log(`im on ${this.position}`);
+    //removeDestination(destination);
   }
 }
 
@@ -32,10 +74,9 @@ class Building {
   }
 }
 
-const elevatorA = new Elevator(
-  position,
-  (basement = false),
-  (penthouse = true)
-);
+const elevatorA = new Elevator(false, 0, false, true, 0);
 
 console.log(Elevator);
+
+elevatorA.addDestination(3);
+console.log();
